a1477cd00e3b0159a6c01494d5f0a8dc2d31c952
moonki <moonki0623@naver.com>
2023-10-17 00:41:28 +0900

    minor test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [img, setImg] = useState(null);
+  const [vid, setVid] = useState(null);
+  const imgUrl =
+    "https://2023-10-16-s3-m3u8.s3.ap-northeast-2.amazonaws.com/dog.jpeg";
+  const videoUrl =
+    "https://2023-10-16-s3-m3u8.s3.ap-northeast-2.amazonaws.com/sample-mp4-file-small.mp4";
+
+  const clickHandler = (type) => {
+    if (type === "img") {
+      setImg(imgUrl);
+    } else {
+      setVid(videoUrl);
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="rowDiv">
+        <div className="colDiv">
+          <h1> IMG</h1>
+          <button onClick={() => clickHandler("img")}>Click Me</button>
+          {img && (
+            <img
+              className="srcDiv"
+              src={img}
+              alt="img"
+            />
+          )}
+        </div>
+        <div className="colDiv">
+          <h1> VIDEO</h1>
+          <button onClick={() => clickHandler("vid")}>Click Me</button>
+          {vid && (
+            <video
+              className="srcDiv"
+              src={vid}
+              controls
+            />
+          )}
+        </div>
+      </div>
     </div>
   );
 }
